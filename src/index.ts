423c9e7352071c442e53b248baec736a294ebd26
alexey.tsurov <alexey.tsurov@acmecryptocorp.io>
2023-05-17 19:04:47 +0300

    fix: not working as i want it

--- src/index.ts
+++ src/index.ts
@@ -1,108 +1,105 @@
-import { createReadStream, createWriteStream } from "fs";
-import pAll from "p-all";
+import { createReadStream, createWriteStream, ReadStream } from "fs";
 import { join } from "path";
 import { createInterface } from "readline";
-const chunkSize = 500 * 1024 * 1024;
-const encoding = "utf8";
+import { comparator, log, memoryUsage } from "./utils";
+const chunkSize = 30 * 1024 * 1024;
 
 const sortAndWrite = (lines: Array<string>, chunkIndex: number): string => {
-  console.log({ msg: "got sortAndWrite" });
-
   // можно вписать временную папку ОСи вместо partitions
-  const chunkPath = join(__dirname, "partitions", `part_${chunkIndex}.txt`);
+  const chunkPath = join(__dirname, "chunks", `part_${chunkIndex}.txt`);
   const writeStream = createWriteStream(chunkPath);
-
-  const sortedLines = lines.sort();
-  writeStream.write(`${sortedLines}\n`);
+  const sortedLines = lines.sort(comparator).join("\n");
+  writeStream.write(sortedLines);
   writeStream.close();
-  console.log({ msg: "complete sortAndWrite", chunkPath });
   return chunkPath;
 };
 
 const writeCompleteFile = async (partitions: Array<string>) => {
+  log("start writing total", { partitions });
   const writeStream = createWriteStream(join(__dirname, "total.txt"));
-  const readLineInterfaces = partitions.map((partition) =>
-    createInterface({
-      input: createReadStream(partition, { encoding }),
-    })
-  );
+  const readStreams: Array<ReadStream> = [];
+  const readLineInterfaces = partitions.map((partition) => {
+    const rs = createReadStream(partition);
+    readStreams.push(rs);
+    return createInterface({
+      input: rs,
+      crlfDelay: Infinity,
+    });
+  });
 
-  const heap: string[] = [];
-  const rlPromises = readLineInterfaces.map((rl) => async () => {
+  memoryUsage();
+  log("start creating heap", { rll: readLineInterfaces.length });
+  const heap: Array<string> = [];
+  for await (const rl of readLineInterfaces) {
     const { value, done } = await rl[Symbol.asyncIterator]().next();
     if (!done) {
       heap.push(value);
     }
-  });
-  await pAll(rlPromises, { concurrency: 5 });
-  heap.sort();
+  }
+  heap.sort(comparator);
 
+  log("start burning heap", heap);
   while (heap.length > 0) {
     const line = heap.shift();
     writeStream.write(`${line}\n`);
 
     // Добавляем следующую строку из соответствующего чанка в кучу
     const rlIndex = heap.length % readLineInterfaces.length;
-    console.log({
-      rlIndex,
-      heap_length: heap.length,
-      rli_length: readLineInterfaces.length,
-    });
-
     const rl = readLineInterfaces[rlIndex];
     const { value, done } = await rl[Symbol.asyncIterator]().next();
     if (!done) {
       heap.push(value);
     }
-    heap.sort();
+    heap.sort(comparator);
   }
 
-  // Закрываем все потоки
   for (const rl of readLineInterfaces) {
     rl.close();
   }
+  for (const rs of readStreams) {
+    rs.close();
+  }
+
   writeStream.close();
-  console.log({ msg: "done writing total" });
 };
 
-(() => {
-  const readStream = createReadStream(join(__dirname, "../largefile.txt"), {
-    encoding,
-  });
+(async () => {
+  log("start");
+  const readStream = createReadStream(join(__dirname, "../smallfile.txt"));
 
+  log("read stream ready");
   const rlInterface = createInterface({ input: readStream });
   let totalSize = 0;
   let chunkIndex = 0;
   let lines: Array<string> = [];
   const chunks: Array<string> = [];
 
+  log("starting reading chunks", { totalSize });
+  memoryUsage();
   rlInterface.on("line", async (line) => {
-    // console.log({
-    //   line,
-    //   length: Buffer.byteLength(line, encoding),
-    //   totalSize,
-    //   chunkSize,
-    // });
-
-    totalSize += Buffer.byteLength(line, encoding);
+    totalSize += Buffer.byteLength(line);
     lines.push(line);
 
-    if (totalSize > chunkSize || chunkIndex > 4) {
+    if (totalSize > chunkSize) {
       chunks.push(sortAndWrite(lines, chunkIndex));
       chunkIndex++;
       lines = [];
       totalSize = 0;
+      memoryUsage();
     }
   });
 
   rlInterface.on("close", async () => {
-    console.log({ msg: "closing" });
+    memoryUsage();
+    log("closing");
     if (lines.length > 0) {
+      log("lines not null");
       chunks.push(sortAndWrite(lines, chunkIndex));
     }
     chunkIndex++;
     lines = [];
     totalSize = 0;
     await writeCompleteFile(chunks);
+    memoryUsage();
   });
 })();
