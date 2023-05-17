a529751da6c92872f382b2874ade298981b2482d
alexey.tsurov <alexey.tsurov@acmecryptocorp.io>
2023-05-17 20:54:58 +0300

    fix: readline memory leak

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { createReadStream, createWriteStream, ReadStream } from "fs";
 import { join } from "path";
 import { createInterface } from "readline";
-import { comparator, log, memoryUsage } from "./utils";
+import { comparator, log } from "./utils";
 const chunkSize = 30 * 1024 * 1024;
 
 const sortAndWrite = (lines: Array<string>, chunkIndex: number): string => {
@@ -18,6 +18,7 @@ const writeCompleteFile = async (partitions: Array<string>) => {
   log("start writing total", { partitions });
   const writeStream = createWriteStream(join(__dirname, "total.txt"));
   const readStreams: Array<ReadStream> = [];
+
   const readLineInterfaces = partitions.map((partition) => {
     const rs = createReadStream(partition);
     readStreams.push(rs);
@@ -27,30 +28,40 @@ const writeCompleteFile = async (partitions: Array<string>) => {
     });
   });
 
-  memoryUsage();
   log("start creating heap", { rll: readLineInterfaces.length });
   const heap: Array<string> = [];
+
+  const rlIterable = [];
   for await (const rl of readLineInterfaces) {
-    const { value, done } = await rl[Symbol.asyncIterator]().next();
+    const rli = rl[Symbol.asyncIterator]();
+    rlIterable.push(rli);
+    const { value, done } = await rli.next();
     if (!done) {
       heap.push(value);
     }
   }
   heap.sort(comparator);
 
-  log("start burning heap", heap);
+  let i = 0;
+  log("start burning heap");
   while (heap.length > 0) {
+    if (i == rlIterable.length) {
+      i = 0;
+    }
     const line = heap.shift();
     writeStream.write(`${line}\n`);
 
-    // Добавляем следующую строку из соответствующего чанка в кучу
-    const rlIndex = heap.length % readLineInterfaces.length;
-    const rl = readLineInterfaces[rlIndex];
-    const { value, done } = await rl[Symbol.asyncIterator]().next();
+    // const rlIndex = heap.length % rlIterable.length;
+    // if (i != rlIndex) {
+    // log("rlindex", i, [...heap.map((h) => h.length)]);
+    //   i = rlIndex;
+    // }
+    const { value, done } = await rlIterable[i].next();
     if (!done) {
       heap.push(value);
     }
     heap.sort(comparator);
+    i++;
   }
 
   for (const rl of readLineInterfaces) {
@@ -65,41 +76,52 @@ const writeCompleteFile = async (partitions: Array<string>) => {
 
 (async () => {
   log("start");
-  const readStream = createReadStream(join(__dirname, "../smallfile.txt"));
+  const chs = [
+    "/home/heavyrain/Documents/GitHub/optiloader/src/chunks/part_0.txt",
+    "/home/heavyrain/Documents/GitHub/optiloader/src/chunks/part_1.txt",
+    "/home/heavyrain/Documents/GitHub/optiloader/src/chunks/part_2.txt",
+    "/home/heavyrain/Documents/GitHub/optiloader/src/chunks/part_3.txt",
+    "/home/heavyrain/Documents/GitHub/optiloader/src/chunks/part_4.txt",
+    "/home/heavyrain/Documents/GitHub/optiloader/src/chunks/part_5.txt",
+    "/home/heavyrain/Documents/GitHub/optiloader/src/chunks/part_6.txt",
+  ];
 
-  log("read stream ready");
-  const rlInterface = createInterface({ input: readStream });
-  let totalSize = 0;
-  let chunkIndex = 0;
-  let lines: Array<string> = [];
-  const chunks: Array<string> = [];
+  await writeCompleteFile(chs);
+  // const readStream = createReadStream(join(__dirname, "../smallfile.txt"));
 
-  log("starting reading chunks", { totalSize });
-  memoryUsage();
-  rlInterface.on("line", async (line) => {
-    totalSize += Buffer.byteLength(line);
-    lines.push(line);
+  // log("read stream ready");
+  // const rlInterface = createInterface({ input: readStream });
+  // let totalSize = 0;
+  // let chunkIndex = 0;
+  // let lines: Array<string> = [];
+  // const chunks: Array<string> = [];
 
-    if (totalSize > chunkSize) {
-      chunks.push(sortAndWrite(lines, chunkIndex));
-      chunkIndex++;
-      lines = [];
-      totalSize = 0;
-      memoryUsage();
-    }
-  });
+  // log("starting reading chunks", { totalSize });
+  // memoryUsage();
+  // rlInterface.on("line", async (line) => {
+  //   totalSize += Buffer.byteLength(line);
+  //   lines.push(line);
 
-  rlInterface.on("close", async () => {
-    memoryUsage();
-    log("closing");
-    if (lines.length > 0) {
-      log("lines not null");
-      chunks.push(sortAndWrite(lines, chunkIndex));
-    }
-    chunkIndex++;
-    lines = [];
-    totalSize = 0;
-    await writeCompleteFile(chunks);
-    memoryUsage();
-  });
+  //   if (totalSize > chunkSize) {
+  //     chunks.push(sortAndWrite(lines, chunkIndex));
+  //     chunkIndex++;
+  //     lines = [];
+  //     totalSize = 0;
+  //     memoryUsage();
+  //   }
+  // });
+
+  // rlInterface.on("close", async () => {
+  //   memoryUsage();
+  //   log("closing");
+  //   if (lines.length > 0) {
+  //     log("lines not null");
+  //     chunks.push(sortAndWrite(lines, chunkIndex));
+  //   }
+  //   chunkIndex++;
+  //   lines = [];
+  //   totalSize = 0;
+  //   await writeCompleteFile(chunks);
+  //   memoryUsage();
+  // });
 })();
